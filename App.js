bab1a5eecf80139c7a8a65fe3aabeb92aa12db34
Jane49-cloud <janendirangu49@gmail.com>
2022-09-05 19:08:45 +0300

    flexbox effect on react native

--- App.js
+++ App.js
@@ -1,22 +1,54 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
-import {
-  useDimentions,
-  useDeviceOrientation,
-} from "@react-native-community/hooks";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <View
         style={{
-          width: "50%",
-          height: "80%",
+          width: 100,
+          height: 100,
           backgroundColor: "yellow",
         }}
-      >
-        <Text style={styles.text}>hello geeks </Text>
-      </View>
+      ></View>
+      <View
+        style={{
+          width: 100,
+          height: 100,
+          backgroundColor: "tomato",
+        }}
+      ></View>
+      <View
+        style={{
+          width: 100,
+          height: 100,
+          backgroundColor: "green",
+        }}
+      ></View>
+      <View
+        style={{
+          width: 100,
+          height: 100,
+          backgroundColor: "gray",
+        }}
+      ></View>
+      <View
+        style={{
+          width: 400,
+          flexShrink: 1, // shrinks so that other items can fit on the screen
+          height: 100,
+          backgroundColor: "greenyellow",
+        }}
+      ></View>
+      <View
+        style={{
+          width: 100,
+          flexShrink: 1, // shrinks so that other items can fit on the screen
+          height: 100,
+          backgroundColor: "greenyellow",
+        }}
+      ></View>
+
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -25,14 +57,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: "row",
     backgroundColor: "white",
     alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    innerHeight: 50,
-    backgroundColor: "orange",
-    color: "gray",
-    margin: 10,
+    justifyContent: "center", //works when there is wrap
+    flexWrap: "wrap",
+    // flexGrow:the same as setting flex
   },
 });
